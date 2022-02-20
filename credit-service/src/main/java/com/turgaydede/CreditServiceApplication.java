d04f61a2ca1079033e2242eb792e52681fae7315
turgay-dede <turgay.dede28@hotmail.com>
2022-02-21 02:26:22 +0300

    credit-service created

---
+++ credit-service/src/main/java/com/turgaydede/CreditServiceApplication.java
@@ -0,0 +1,11 @@
+package com.turgaydede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CreditServiceApplication.class,args);
+    }
+}

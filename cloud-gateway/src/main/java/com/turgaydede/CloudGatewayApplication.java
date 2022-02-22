ba77d43d17e0e8863e4b2b5ef33c1d53ab1a966c
turgay-dede <turgay.dede28@hotmail.com>
2022-02-23 02:17:05 +0300

    cloud gateway created

---
+++ cloud-gateway/src/main/java/com/turgaydede/CloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.turgaydede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class,args);
+    }
+}

a00b89e71c5e998ecfec2e4cd2cb120c78b701b5
turgay-dede <turgay.dede28@hotmail.com>
2022-02-22 21:07:49 +0300

    credit score service and setCreditScore method created

---
+++ credit-service/src/main/java/com/turgaydede/business/abstracts/CreditScoreService.java
@@ -0,0 +1,7 @@
+package com.turgaydede.business.abstracts;
+
+import com.turgaydede.entities.CreditScore;
+
+public interface CreditScoreService {
+    CreditScore setCreditScore(String identityNumber);
+}

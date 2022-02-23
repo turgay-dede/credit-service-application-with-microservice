6d7afa0774af7082c304d65e296a1991baed0951
turgay-dede <turgay.dede28@hotmail.com>
2022-02-23 23:58:21 +0300

    Builder pattern added customer dto

--- common-service/src/main/java/com/turgaydede/dtos/CustomerDto.java
+++ common-service/src/main/java/com/turgaydede/dtos/CustomerDto.java
@@ -1,5 +1,7 @@
 package com.turgaydede.dtos;
 
+import java.util.Objects;
+
 public class CustomerDto {
     private int id;
     private String fullName;
@@ -7,7 +9,12 @@ public class CustomerDto {
     private int monthlyIncome;
     private String phoneNumber;
 
-    public CustomerDto() {
+    public CustomerDto(Builder builder) {
+        this.id = builder.id;
+        this.fullName = builder.fullName;
+        this.identityNumber = builder.identityNumber;
+        this.monthlyIncome = builder.monthlyIncome;
+        this.phoneNumber = builder.phoneNumber;
     }
 
     public CustomerDto(int id, String fullName, String identityNumber, int monthlyIncome, String phoneNumber) {
@@ -67,5 +74,56 @@ public class CustomerDto {
                 ", monthlyIncome=" + monthlyIncome +
                 ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomerDto that = (CustomerDto) o;
+        return id == that.id && monthlyIncome == that.monthlyIncome && Objects.equals(fullName, that.fullName) && Objects.equals(identityNumber, that.identityNumber) && Objects.equals(phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, identityNumber, monthlyIncome, phoneNumber);
+    }
+
+    public static class Builder {
+        private int id;
+        private String fullName;
+        private String identityNumber;
+        private int monthlyIncome;
+        private String phoneNumber;
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder fullName(String fullName) {
+            this.fullName = fullName;
+            return this;
+        }
+
+        public Builder identityNumber(String identityNumber) {
+            this.identityNumber = identityNumber;
+            return this;
+        }
+
+        public Builder monthlyIncome(int monthlyIncome) {
+            this.monthlyIncome = monthlyIncome;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public CustomerDto build() {
+            return new CustomerDto(this);
+        }
+
     }
 }

5c1563aca431dcfeeec1952729749867a71a36ac
turgay-dede <turgay.dede28@hotmail.com>
2022-02-26 02:59:44 +0300

    update test created

--- credit-service/src/test/java/com/turgaydede/business/concreates/CreditServiceImplTest.java
+++ credit-service/src/test/java/com/turgaydede/business/concreates/CreditServiceImplTest.java
@@ -3,6 +3,7 @@ package com.turgaydede.business.concreates;
 import com.turgaydede.business.abstracts.CreditScoreService;
 import com.turgaydede.entities.Credit;
 import com.turgaydede.entities.CreditConsent;
+import com.turgaydede.entities.dtos.CreditDto;
 import com.turgaydede.feign.customer.CustomerFeignClient;
 import com.turgaydede.repositories.CreditRepository;
 import com.turgaydede.util.converter.CreditDtoConverter;
@@ -14,7 +15,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CreditServiceImplTest {
@@ -36,9 +39,34 @@ class CreditServiceImplTest {
         verify(creditRepository, Mockito.times(1)).delete(credit);
     }
 
+    @Test
+    void update() {
+        CreditDto creditDto = generateConfirmCreditDto();
+        Credit credit = Credit.builder()
+                .id(creditDto.getId())
+                .identityNumber(creditDto.getIdentityNumber())
+                .creditConsent(creditDto.getCreditConsent())
+                .creditLimit(creditDto.getCreditLimit())
+                .build();
+
+        when(creditRepository.save(credit)).thenReturn(credit);
+
+        CreditDto result = creditServiceImpl.update(creditDto).getData();
+        assertNotEquals(result, creditDto);
+        verify(creditRepository).save(Mockito.any(Credit.class));
+    }
 
     private Credit generateConfirmCredit(){
         return Credit.builder()
+                .id("1")
+                .identityNumber("20000000000")
+                .creditConsent(CreditConsent.CONFIRM)
+                .creditLimit(10000)
+                .build();
+    }
+
+    private CreditDto generateConfirmCreditDto(){
+        return CreditDto.builder()
                 .id("1")
                 .identityNumber("20000000000")
                 .creditConsent(CreditConsent.CONFIRM)

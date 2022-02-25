c40a66571e9a11260a2ff46eb7d532f9e42e95c0
turgay-dede <turgay.dede28@hotmail.com>
2022-02-26 02:07:37 +0300

    getByCustomerForIdentityNumber test created

--- customer-service/src/test/java/com/turgaydede/business/concreates/CustomerServiceImplTest.java
+++ customer-service/src/test/java/com/turgaydede/business/concreates/CustomerServiceImplTest.java
@@ -4,7 +4,6 @@ import com.turgaydede.entities.Customer;
 import com.turgaydede.entities.dtos.CustomerDto;
 import com.turgaydede.repositories.CustomerRepository;
 import com.turgaydede.util.converter.CustomerDtoConverter;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,6 +16,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -36,15 +37,12 @@ class CustomerServiceImplTest {
         Customer customer = generateCustomer();
         CustomerDto customerDto = generateCustomerDto();
 
-
-        Mockito.when(customerRepository.save(customer)).thenReturn(customer);
-        Mockito.when(customerDtoConverter.convert(customer)).thenReturn(customerDto);
-
+        when(customerRepository.save(customer)).thenReturn(customer);
+        when(customerDtoConverter.convert(customer)).thenReturn(customerDto);
 
         CustomerDto result = customerServiceImpl.add(customerDto).getData();
-        Assertions.assertEquals(result, customerDto);
-
-        Mockito.verify(customerDtoConverter).convert(customer);
+        assertEquals(result, customerDto);
+        verify(customerDtoConverter).convert(customer);
     }
 
     @Test
@@ -53,13 +51,12 @@ class CustomerServiceImplTest {
 
         customerRepository.delete(customer);
 
-        Mockito.verify(customerRepository, Mockito.times(1)).delete(customer);
+        verify(customerRepository, Mockito.times(1)).delete(customer);
     }
 
     @Test
     void update() {
         CustomerDto customerDto = generateCustomerDto();
-
         Customer customer = Customer.builder()
                 .id(customerDto.getId())
                 .fullName(customerDto.getFullName())
@@ -68,13 +65,11 @@ class CustomerServiceImplTest {
                 .phoneNumber(customerDto.getPhoneNumber())
                 .build();
 
-        Mockito.when(customerRepository.save(customer)).thenReturn(customer);
+        when(customerRepository.save(customer)).thenReturn(customer);
 
-        CustomerDto updatedCustomerDto = customerServiceImpl.update(customerDto).getData();
-
-        Assertions.assertNotEquals(updatedCustomerDto,customerDto);
-
-        Mockito.verify(customerRepository).save(Mockito.any(Customer.class));
+        CustomerDto result = customerServiceImpl.update(customerDto).getData();
+        assertNotEquals(result, customerDto);
+        verify(customerRepository).save(Mockito.any(Customer.class));
     }
 
     @Test
@@ -85,11 +80,32 @@ class CustomerServiceImplTest {
         customerList.add(customer1);
         customerList.add(customer2);
 
-        Mockito.when(customerRepository.findAll()).thenReturn(customerList);
+        when(customerRepository.findAll()).thenReturn(customerList);
 
-        List<CustomerDto> customerDtoList = customerServiceImpl.getAll().getData();
+        List<CustomerDto> result = customerServiceImpl.getAll().getData();
+        assertEquals(customerList.size(), result.size());
+    }
 
-        Assertions.assertEquals(customerList.size(),customerDtoList.size());
+    @Test
+    void getByCustomerForIdentityNumber() {
+        String identityNumber = "20000000000";
+        CustomerDto customerDto = generateCustomerDto();
+        Customer customer = Customer.builder()
+                .id(1)
+                .fullName("Turgay Dede")
+                .identityNumber("20000000000")
+                .monthlyIncome(8000)
+                .phoneNumber("5400000000")
+                .build();
+
+        when(customerRepository.findByIdentityNumber(identityNumber)).thenReturn(Optional.of(customer));
+        when(customerDtoConverter.convert(customer)).thenReturn(customerDto);
+
+        CustomerDto result = customerServiceImpl.getByCustomerForIdentityNumber(customer.getIdentityNumber()).getData();
+
+        assertEquals(result, customerDto);
+        verify(customerDtoConverter).convert(customer);
+        verify(customerRepository).findByIdentityNumber(identityNumber);
     }
 
 

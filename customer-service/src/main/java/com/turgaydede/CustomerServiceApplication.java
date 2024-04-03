d7afbf34e81328b65313b1f5d54d974109fbdccb
tdede <turgay.dede28@hotmail.com>
2024-04-03 22:51:57 +0300

    customer-service enable eureka client

--- customer-service/src/main/java/com/turgaydede/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/turgaydede/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package com.turgaydede;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class,args);
